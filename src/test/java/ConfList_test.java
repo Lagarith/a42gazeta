10834768d7aec64d8db5fa0816d3e64b5aff675a
Lagarith <lagarit@gmail.com>
2017-07-11 16:17:15 +0700

    Add ConfList_test.java

---
+++ src/test/java/ConfList_test.java
@@ -0,0 +1,84 @@
+/** ConfList_test */
+
+import Helpers.PageElement;
+import Helpers.Settings;
+import Helpers.Locators.*;
+import com.galenframework.config.GalenConfig;
+import com.galenframework.config.GalenProperty;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.sun.deploy.ref.Helpers;
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.*;
+import org.openqa.selenium.internal.WrapsDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.testng.annotations.BeforeTest;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.shooting.RotatingDecorator;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategy;
+import ru.yandex.qatools.ashot.shooting.ViewportPastingDecorator;
+import ru.yandex.qatools.ashot.shooting.cutter.CutStrategy;
+import ru.yandex.qatools.ashot.shooting.cutter.VariableCutStrategy;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.imageio.ImageIO;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import static Helpers.Locators.*;
+import static Helpers.PageElement.*;
+
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ConfList_test extends Settings {
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        System.setProperty("galen.config", "C:\\tests\\a42gazeta\\galen.config");
+        GalenConfig.getConfig().setProperty(GalenProperty.SCREENSHOT_FULLPAGE, "true");
+        SetBrowserFirefox();
+        browser.manage().window().setSize(new Dimension(1366, 768));
+    }
+
+
+
+    @Test
+    public void a_NewsList_load_test() throws Exception {
+        LoadPageAndVerify(AuthorsListURL);
+    }
+
+
+
+    @AfterClass
+    public static void setDown() throws Exception {
+        browser.quit();
+    }
+}
+
+
