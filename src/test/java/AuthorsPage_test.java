d9a29edd8f316011cfa0670865bf67edf3e28318
Lagarith <lagarit@gmail.com>
2017-07-12 16:58:19 +0700

    add AuthorsPage_test

---
+++ src/test/java/AuthorsPage_test.java
@@ -0,0 +1,63 @@
+/** AuthorsPage_test */
+import Helpers.Settings;
+import com.galenframework.config.GalenConfig;
+import com.galenframework.config.GalenProperty;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.*;
+
+import static Helpers.Locators.*;
+import static Helpers.PageElement.*;
+
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AuthorsPage_test extends Settings{
+
+    private static String[][] data = new String[23][2];
+
+    private static String[][] getLinks() throws Exception {
+        browser.get(AuthorsListURL);
+
+        Click(NewsList_ShowMore_btn);
+
+        for (int i = 1; i < 23; i++) {
+            String xPath = "//*[@id=\"paginate-block\"]/div/div[" + i + "]/p[1]/a/strong";
+            String xPath2 = "//*[@id=\"paginate-block\"]/div/div[" + i + "]/p[1]/a";
+
+            data[i][0] = browser.findElement(By.xpath("//*[@id=\"paginate-block\"]/div/div[" + i + "]/p[1]/a/strong")).getText();
+            data[i][1] = browser.findElement(By.xpath("//*[@id=\"paginate-block\"]/div/div[" + i + "]/p[1]/a")).getAttribute("href");
+                                                       //*[@id="paginate-block"]/div/div[22]/p/a
+        }
+
+        return data;
+    }
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        System.setProperty("galen.config", "C:\\tests\\a42gazeta\\galen.config");
+        GalenConfig.getConfig().setProperty(GalenProperty.SCREENSHOT_FULLPAGE, "true");
+        SetBrowserFirefox();
+        browser.manage().window().setSize(new Dimension(1366, 768));
+        data = getLinks();
+    }
+
+
+
+    @Test
+    public void a_Open_Authors_Page_test() throws Exception {
+        for (String[] item:data) {
+            LoadPageAndVerify(item[1]);
+        }
+    }
+
+
+    @AfterClass
+    public static void setDown() throws Exception {
+        browser.quit();
+    }
+}
+
